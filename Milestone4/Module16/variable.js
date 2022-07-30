77e6115eee0d5cc4e94c9a4d7144b9748f96c4e9
Abir Hasan <cse.170201063@gmail.com>
2022-07-31 02:41:26 +0600

    16.4 variable type numeric string boolean

--- Milestone4/Module16/variable.js
+++ Milestone4/Module16/variable.js
@@ -1,6 +1,14 @@
 // variable
 
+// numeric
 var price = 30;
 var age = 21;
 var temperature = 30;
 
+// string
+var person = 'Jahanara Tisha';
+var location = 'Gobindoganj Gaibandha';
+
+// boolean
+var love = true;
+var love_return = false;
