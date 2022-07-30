9a727319ff283da0aab49433356ad4aa36397532
Abir Hasan <cse.170201063@gmail.com>
2022-07-31 02:18:04 +0600

    16.3 variable

---
+++ Milestone4/Module16/math.js
@@ -0,0 +1,11 @@
+// Integer number. 2, 3, 5
+// Float number. 1.3, 1.5
+// Operation: addition, multiplication, subtraction, division, reminder
+// Percentage
+// Average
+// Comparison
+// Oikik niom  math
+// Max, min, ava. sum
+// series
+// Odd, even. 
+// Prime number, permutation, 
---
+++ Milestone4/Module16/variable.js
@@ -0,0 +1,6 @@
+// variable
+
+var price = 30;
+var age = 21;
+var temperature = 30;
+
