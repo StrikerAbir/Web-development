b46dded31db02c9303d54c7d098be74727b8f7b1
Abir Hasan <cse.170201063@gmail.com>
2022-07-31 15:43:19 +0600

    16.8 concatenation, type conversion

---
+++ Milestone4/Module16/concatenation.js
@@ -0,0 +1,12 @@
+var a = 10;
+var b = 30;
+var total = a + b;
+console.log(total);
+
+// string concatenation 
+var firstName = 'jahanara';
+var lastName = 'tisha';
+var fullName = firstName + lastName;
+console.log(fullName);
+var fullName = firstName + ' ' + lastName;
+console.log(fullName);
---
+++ Milestone4/Module16/conversion.js
@@ -0,0 +1,16 @@
+var a = '30';
+var b = '63';
+var total = a + b;
+console.log(total);
+var a = '30';
+var b = 63;
+var total = a + b;
+console.log(total);
+
+// type conversion
+
+var c = parseInt(a)
+console.log(c);
+var e= '4.32'
+var d = parseFloat(e);
+console.log(d);
