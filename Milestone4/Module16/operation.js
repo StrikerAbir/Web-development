5ff287d374aecc645c098ab29a3850711e2dae82
Abir Hasan <cse.170201063@gmail.com>
2022-07-31 14:22:41 +0600

    16.6 simple mathmetical operatiom

---
+++ Milestone4/Module16/operation.js
@@ -0,0 +1,15 @@
+var onionPrice = 20;
+var eggPrice = 10;
+console.log(eggPrice);
+console.log(onionPrice);
+console.log(onionPrice + eggPrice);
+var total = onionPrice + eggPrice;
+console.log('total egg and onion =',total);
+var difference = onionPrice - eggPrice;
+console.log('difference price =', difference);
+var orangePrice = 20;
+var quantity = 7;
+var multiply = orangePrice * quantity;
+console.log('multiplier =', multiply);
+var division = multiply / quantity;
+console.log('price of single orange =',division);
