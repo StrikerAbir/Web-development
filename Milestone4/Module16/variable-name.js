7c3a1f49d5f12775fcd2e2d09c3217d98a4dcb96
Abir Hasan <cse.170201063@gmail.com>
2022-07-31 14:09:39 +0600

    16.5 keyword and variable name

---
+++ Milestone4/Module16/variable-name.js
@@ -0,0 +1,5 @@
+// Variable name can not be any keywords. 
+// Variable naem has to be in one word
+// Variable name can not have quotation
+// Variable name can not starts with a  number 
+// variable name case sensetive 
