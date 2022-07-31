b94040f001f27b7b979c21a6c2191a825e3332c5
Abir Hasan <cse.170201063@gmail.com>
2022-07-31 17:23:25 +0600

    todo practice

---
+++ Milestone4/Module16/practice.js
@@ -0,0 +1,29 @@
+// problem 1
+var taka = 1000;
+console.log('given taka by mom =', taka);
+var price = 700;
+console.log('given to shopkeeper =', price);
+var taka_return = taka - price;
+console.log('taka returned =', taka_return);
+
+// problem 2
+var a = 75.25;
+var b = 65;
+var c = 80;
+var d = 35.45;
+var e = 99.50;
+var total = a + b + c + d + e;
+var Average = (total / 5).toFixed(2);
+console.log('avarage of numbers =', Average);
+
+// problem 3
+var st1 = 'i am going to';
+var st2 = 'love tishu forever.';
+var line = st1 + ' ' + st2;
+console.log(line);
+
+// problem 4
+var num = 119;
+var divisor = 5;
+var reminder = num % divisor;
+console.log(reminder);
