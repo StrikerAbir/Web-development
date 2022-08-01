9221ea0b76cd3c6d9ba6a04f98aa85902f24eeed
Abir Hasan <cse.170201063@gmail.com>
2022-08-01 15:12:39 +0600

    17.7 multiple condition

---
+++ Milestone4/Module17/multi-conditon.js
@@ -0,0 +1,8 @@
+var isGrade = true;
+var Salary = 35000;
+var car = 1;
+if (isGrade == true && Salary > 50000 || car>=1) {
+    console.log('you can marry.');
+} else {
+    console.log('die single forever');
+}
