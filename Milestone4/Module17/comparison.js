0f022c075224dcfe0542bf6db8d529a450ffd354
Abir Hasan <cse.170201063@gmail.com>
2022-08-01 14:58:01 +0600

    17.5 comparison

---
+++ Milestone4/Module17/comparison.js
@@ -0,0 +1,11 @@
+console.log(5 < 6);
+console.log(5 == 5);
+console.log(5 > 6);
+console.log(5 != 6);
+console.log(5 <= 6);
+console.log(5 >= 6);
+
+var num1 = 30;
+var num2 = 63;
+console.log(num1 < num2);
+
