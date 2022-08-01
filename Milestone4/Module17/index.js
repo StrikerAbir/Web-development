836ecf35ede52cf7223c8eabf9055bdbbe21db26
Abir Hasan <cse.170201063@gmail.com>
2022-08-01 14:15:25 +0600

    17.3 array index, get and set by index, indexOf

---
+++ Milestone4/Module17/index.js
@@ -0,0 +1,12 @@
+var number = [12, 34, 67, 87, 66, 33];
+console.log(number);
+console.log(number[2]);
+//get elements by index
+var element = number[1];
+console.log(element);
+// set elements by index
+number[2] = 2;
+console.log(number[2])
+// find the index of an element
+var position = number.indexOf(87);
+console.log(position);
