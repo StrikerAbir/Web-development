270cf85d93d077403e62303ca36dec083097ca75
Abir Hasan <cse.170201063@gmail.com>
2022-08-01 14:30:09 +0600

    17.4 push pop

---
+++ Milestone4/Module17/push-pop.js
@@ -0,0 +1,18 @@
+var number = [12, 34, 67, 87, 66, 30];
+console.log(number);
+// push
+number.push(63);
+console.log(number)
+// pop
+number.pop();
+number.pop();
+console.log(number);
+// pop into an variable
+var element = number.pop();
+console.log(element);
+
+// push pop in start position
+number.unshift(30);
+console.log(number);
+number.shift();
+console.log(number);
