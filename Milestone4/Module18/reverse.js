8a7f6fba8be06107ccd0421fef4898854c4ed5af
Abir Hasan <cse.170201063@gmail.com>
2022-08-02 15:52:29 +0600

    quiz

--- Milestone4/Module18/reverse.js
+++ Milestone4/Module18/reverse.js
@@ -6,3 +6,12 @@ while (num>0) {
 for (var i = 10; i > 0; i--){
     console.log(i);
 }
+
+var i = 0;
+for (i=0; i<5; i++){};
+console.log(i);
+// is it wrong or right
+var i = 4;
+for ( i < 5; i++) {
+  console.log(i);
+}
