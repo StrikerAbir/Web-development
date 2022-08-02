83a6ef4cb61eb19ac97a2fc2b7197e0241e5aaf7
Abir Hasan <cse.170201063@gmail.com>
2022-08-02 14:38:18 +0600

    18.4 for loop

---
+++ Milestone4/Module18/forloop.js
@@ -0,0 +1,13 @@
+
+for (var i = 0; i < 7; i++) {
+    console.log(i);
+}
+
+//even
+for (var i = 0; i < 7; i+=2) {
+    console.log(i);
+}
+// odd
+for (var i = 1; i < 7; i+=2) {
+    console.log(i);
+}
