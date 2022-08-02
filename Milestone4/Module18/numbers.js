46b6a56a99aedf507a398b6d5fd3c6acd7965609
Abir Hasan <cse.170201063@gmail.com>
2022-08-02 14:26:18 +0600

    18.3 loop example

---
+++ Milestone4/Module18/numbers.js
@@ -0,0 +1,19 @@
+var num = 0;
+while (num <= 10) {
+    console.log(num);
+    num++;
+}
+
+// even
+var even = 0;
+while (even <= 10) {
+    console.log(even); 
+    even += 2;
+}
+
+// odd
+var Odd = 1;
+while (Odd <= 10) {
+    console.log(Odd); 
+    Odd += 2;
+}
