3e6cb7bcbbaa146040947929ca11fd664e0e3b53
Abir Hasan <cse.170201063@gmail.com>
2022-08-04 14:36:25 +0600

    19.6 object

---
+++ Milestone4/Module19/object.js
@@ -0,0 +1,14 @@
+// object declearation
+var student = {
+    id: 170201030,
+    name: 'Jahanara Tisha',
+    year: '4th',
+    loved_by: 'Abir'
+}
+console.log(student);
+console.log(student.name);
+console.log(student.loved_by);
+student.year = 'graduated';
+console.log(student);
+
+
