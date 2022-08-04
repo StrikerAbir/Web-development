f7058a0edebf9a9a348f5854cc1fbf855c210d18
Abir Hasan <cse.170201063@gmail.com>
2022-08-04 15:30:39 +0600

    19.7 object property

---
+++ Milestone4/Module19/object-property.js
@@ -0,0 +1,33 @@
+var shoppingcart = {
+    books: 3,
+    sunglasses: 1,
+    keyboard: 7,
+    mouse: 8,
+    pen: 10
+}
+console.log(shoppingcart);
+
+// when you know the property name, use dot notation to get the property value
+var pencount = shoppingcart.pen;
+console.log(pencount);
+// alternative way to get the property
+var pencount = shoppingcart['pen'];
+// to get keys name from object
+var property = Object.keys(shoppingcart);
+console.log(property)
+// to get the property value from object
+var propertyvalue = Object.values(shoppingcart);
+console.log(propertyvalue)
+
+// when property name in a variable
+var propertyname = 'keyboard';
+var property = shoppingcart[propertyname];
+console.log(property)
+
+// set property value
+shoppingcart.mouse = 20;
+console.log(shoppingcart);
+
+shoppingcart['keyboard'] = 2;
+console.log(shoppingcart);
+ 
