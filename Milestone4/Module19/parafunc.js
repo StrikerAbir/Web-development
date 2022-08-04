0ee577efccab90fc8f22a7d1f965b689fa427a86
Abir Hasan <cse.170201063@gmail.com>
2022-08-04 13:05:28 +0600

    19.3 multipara function

---
+++ Milestone4/Module19/parafunc.js
@@ -0,0 +1,15 @@
+function go(location) {
+    console.log('go to ',location);
+}
+
+go('bandarban');
+
+function add(a,b) {
+    console.log('add ',a+b);
+}
+add(4, 5);
+
+function add(a,b,c) {
+    console.log('add ',a+b);
+}
+add(4,5);
