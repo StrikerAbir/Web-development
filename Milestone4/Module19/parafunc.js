bc9e44a2714e42e70dc699d55d7ef9a1f05c5c26
Abir Hasan <cse.170201063@gmail.com>
2022-08-04 13:34:52 +0600

    19.4 retun keyword

--- Milestone4/Module19/parafunc.js
+++ Milestone4/Module19/parafunc.js
@@ -10,6 +10,6 @@ function add(a,b) {
 add(4, 5);
 
 function add(a,b,c) {
-    console.log('add ',a+b);
+    console.log('add ',a+b+c);
 }
 add(4,5);
---
+++ Milestone4/Module19/return.js
@@ -0,0 +1,6 @@
+function sum(a, b) {
+    var add = a + b;
+    return add;
+}
+var addition = sum(5, 5);
+console.log(addition);
