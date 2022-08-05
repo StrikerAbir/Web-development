f29b3c2d75bdc2774de766f9b7d545831271f337
Abir Hasan <cse.170201063@gmail.com>
2022-08-05 18:30:30 +0600

    20.8 factorial using while loop or decriment loop

--- Milestone4/Module20/factorial.js
+++ Milestone4/Module20/factorial.js
@@ -7,3 +7,25 @@ function factorial(a) {
     return value;
 }
 console.log(factorial(4));
+
+// reverse loop
+function factorial(a) {
+    let value=1;
+    for (let i = a; i >= 0; i--){
+        value *= i;
+    }
+    return value;
+}
+console.log(factorial(4));
+
+// while loop
+function factorial(a) {
+    let value = 1;
+    let i = 1;
+    while (a >= i) {
+        value *= i;
+        i++;
+    }
+    return value;
+}
+console.log(factorial(4));
