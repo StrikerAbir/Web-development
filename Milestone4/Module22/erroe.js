ec42044e9f80252616dc259d1231dd36d40eeaff
Abir Hasan <cse.170201063@gmail.com>
2022-08-09 23:53:25 +0600

    22.9

--- Milestone4/Module22/erroe.js
+++ Milestone4/Module22/erroe.js
@@ -8,3 +8,14 @@ function add(num1, num2) {
 }
 console.log(add(12, 45));
 console.log(add(12, '45'));
+
+const nayoks = ['Bangla Bhai', 'English Bhai', 'Korean Bhai ', 'Turkish Bhai '];
+for (const nayok in nayoks) {
+    console.log(nayoks[nayok]);
+}
+
+let sum=0; 
+for( let i = 0; i<=3;i++){ 
+sum = sum + i; 
+}
+console.log(sum)
