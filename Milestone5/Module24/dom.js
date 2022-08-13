c93b8a05b6862cf5e0502f8ff6339f4ff9b7e4be
Abir Hasan <cse.170201063@gmail.com>
2022-08-13 15:09:40 +0600

    24.2 traversing dom: getElementByTagName

--- Milestone5/Module24/dom.js
+++ Milestone5/Module24/dom.js
@@ -1,3 +1,10 @@
 console.log('hello tishu my love');
 console.log(document);
 console.log(document.body);
+console.log(document.getElementsByTagName('h1'));
+const h1collection = document.getElementsByTagName('h1');
+for (const h1 of h1collection) {
+    console.log(h1);
+    console.log(h1.innerText);
+}
+
