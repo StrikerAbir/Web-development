57b1e1c32e61b217e5c6818b7ed19b8c7a8314ef
Abir Hasan <cse.170201063@gmail.com>
2022-08-13 18:52:29 +0600

    24.6 styling dom properties, add and remove class using js

---
+++ Milestone5/Module24/style.js
@@ -0,0 +1,23 @@
+// 24.6
+const sections = document.querySelectorAll('section');
+console.log(sections);
+for (const section of sections) {
+    console.log(section)
+    section.style.border = '2px solid blue';
+    section.style.marginBottom = '5px';
+    section.style.borderRadius = '5px';
+    section.style.paddingLeft = '7px';
+    section.style.backgroundColor = 'lightgray';
+}
+
+const placecontainer = document.getElementById('places-container');
+placecontainer.style.backgroundColor = "yellow";
+
+const domcontainer = document.getElementById('dom-container');
+/*
+domcontainer.classList.add('yellow-bg'); //ekhane o/p gray e dekhasse cz yellow-bg k oberride kore disse. class dia korar theke inline kora beshi power tai emn hoise. inspect a gia dekhlei bojha jai. just dekhar jonne css file a !important use kora holo.
+*/
+
+
+domcontainer.classList.add('text-center');
+domcontainer.classList.remove('large-text');
