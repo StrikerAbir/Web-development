6096bad325af09b4564697ddf9ed87642ac957b1
Abir Hasan <cse.170201063@gmail.com>
2022-08-13 18:24:28 +0600

    24.5 set and get Attributes, innerHTML, innerText

--- Milestone5/Module24/dom.js
+++ Milestone5/Module24/dom.js
@@ -63,3 +63,34 @@ for (const li of someli) {
 // querySelector
 console.log(document.querySelector('.fruits-container li')) //only show first list
 */
+
+
+// 24.5
+/*
+    console.log(document.getElementById('fruitstitle'));
+    console.log(document.getElementById('fruitstitle').style);
+    document.getElementById('fruitstitle').style.backgroundColor='red'; //red kore dilo bg
+
+    //getAttribute
+    const title=document.getElementById('fruitstitle');
+    console.log(title.getAttribute('id'));
+    console.log(title.getAttribute('class'));
+    // classlist
+    console.log(title.classList);
+    title.classList.add('blue');
+    console.log(title.classList);
+    title.classList.remove('blue');
+    console.log(title.classList);
+    // setAttribute
+    title.setAttribute('title', 'tooltip added by js'); //fruits title a cursor rakhno dekhabe. 
+
+    // innerHTML
+    console.log(document.getElementsByClassName('fruits-container'));
+    console.log(document.getElementsByClassName('fruits-container')[0]); //[0] mane 1st index bojaise. 1st class ta rki.
+    console.log(document.getElementsByClassName('fruits-container')[0].innerHTML);
+    // set new innerHTML
+    console.log(document.getElementsByClassName('fruits-container')[0].innerHTML='<h1>DOM Changed this</h1>');
+
+    // innerText
+    console.log(document.getElementsByClassName('fruits-container')[0].innerText);
+    */
