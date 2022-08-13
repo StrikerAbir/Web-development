8ca715926067791ec64b328f3d9879a6add0b94b
Abir Hasan <cse.170201063@gmail.com>
2022-08-13 16:21:14 +0600

    24.4 traversing dom- getelementby id nd byclassname

--- Milestone5/Module24/dom.js
+++ Milestone5/Module24/dom.js
@@ -1,4 +1,4 @@
-console.log('hello tishu my love');
+/*console.log('hello tishu my love');
 console.log(document);
 console.log(document.body);
 console.log(document.getElementsByTagName('h1'));
@@ -7,4 +7,40 @@ for (const h1 of h1collection) {
     console.log(h1);
     console.log(h1.innerText);
 }
+const student = {
+    name: 'abul',
+    id: 11,
+    study: function (time) {
+        console.log(time, 'study running')
+    }
+}
 
+student.study(5);
+console.log(document.getElementsByTagName('li'));
+console.log('inside the Html');
+const licollection = document.getElementsByTagName('li');
+console.log(licollection);
+for (const li of licollection) {
+    console.log(li);
+}
+for (const li of licollection) {
+    console.log(li.innerText);
+}*/
+
+/*
+    // getElementById
+    console.log(document.getElementById('fruitstitle'));
+    console.log(document.getElementById('placestitle'));
+    console.log(document.getElementById('placestitle').innerText); //inner text select
+    console.log(document.getElementById('placestitle').innerText='my tour list'); //inner text changed.
+    console.log(document.getElementById('fruit')); //id didnt used in html. it will give null.
+
+    // getElementsByClassName
+    console.log(document.getElementsByClassName('impo-places'));
+    const places= document.getElementsByClassName('impo-places');
+    for(const place of places){
+        console.log(place);
+        console.log(place.innerText);
+    }
+    console.log(document.getElementsByClassName('places')); //class didnt used in html. it will give empty array type object.
+    */
