d21fa437a5b1cc71da4189fcd03c7e6c9ba35798
Abir Hasan <cse.170201063@gmail.com>
2022-08-17 02:55:50 +0600

    27.6 advanced share one function between two i/p field

--- Milestone5/Module27/My Bank Functional/deposit.js
+++ Milestone5/Module27/My Bank Functional/deposit.js
@@ -1,3 +1,23 @@
+function getInputFieldValueId(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    if (inputFieldValueString == '') {
+        alert('Please enter Amount.')
+        return;
+    }
+    inputField.value = '';
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    return inputFieldValue;
+}
+
+function getElementValueId(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
+}
+
 document.getElementById('btn-deposit').addEventListener('click', function () {
+    const newDepositAmount = getInputFieldValueId('user-deposit');
     
 })
