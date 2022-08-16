63b1b824395bcaccce5a8c06ce81cf624629898a
Abir Hasan <cse.170201063@gmail.com>
2022-08-17 03:25:43 +0600

    27.7 advanced get and set value using function

--- Milestone5/Module27/My Bank Functional/deposit.js
+++ Milestone5/Module27/My Bank Functional/deposit.js
@@ -1,23 +1,19 @@
-function getInputFieldValueId(inputFieldId) {
-    const inputField = document.getElementById(inputFieldId);
-    const inputFieldValueString = inputField.value;
-    if (inputFieldValueString == '') {
-        alert('Please enter Amount.')
-        return;
-    }
-    inputField.value = '';
-    const inputFieldValue = parseFloat(inputFieldValueString);
-    return inputFieldValue;
-}
-
-function getElementValueId(elementId) {
-    const element = document.getElementById(elementId);
-    const elementValueString = element.innerText;
-    const value = parseFloat(elementValueString);
-    return value;
-}
 
 document.getElementById('btn-deposit').addEventListener('click', function () {
     const newDepositAmount = getInputFieldValueId('user-deposit');
+    if (newDepositAmount != 0) {
+        // calculate new deposit amount
+        const previousDepositTotal = getElementValueId('deposit-total');
+        const depositSum = newDepositAmount + previousDepositTotal;
+        // set new deposit total amount
+        setTextElementValueId('deposit-total', depositSum);
+
+        // calculate new balance amount
+        const previousBalanceTotal = getElementValueId('balance-total');
+        const balanceSum = newDepositAmount + previousBalanceTotal;
+        // set new balance amount
+        setTextElementValueId('balance-total', balanceSum);
+    }
+    
     
 })
--- Milestone5/Module27/My Bank Functional/utilities.js
+++ Milestone5/Module27/My Bank Functional/utilities.js
@@ -1,1 +1,27 @@
-const hi = 'tisha loves abir';
+function getInputFieldValueId(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    console.log('in1')
+    const inputFieldValueString = inputField.value;
+    if (inputFieldValueString == '') {
+        alert('Please enter Amount.')
+        return 0;
+    } else {
+        inputField.value = '';
+        const inputFieldValue = parseFloat(inputFieldValueString);
+        return inputFieldValue;
+    }
+}
+
+function getElementValueId(elementId) {
+    console.log('in2')
+    const textElement = document.getElementById(elementId);
+    const elementValueString = textElement.innerText;
+    const previousValue = parseFloat(elementValueString);
+    return previousValue;
+}
+
+function setTextElementValueId(elementId, newValue) {
+    console.log('in3')
+    const textElement = document.getElementById(elementId);
+    textElement.innerText = newValue;
+}
