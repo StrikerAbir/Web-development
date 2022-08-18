2b5d0b9a51ff678e472b2b7f90cb168e11b98a75
Abir Hasan <cse.170201063@gmail.com>
2022-08-18 10:10:34 +0600

    calculator practice done

--- Milestone5/Module27.5/practice/Calculator/calculator.js
+++ Milestone5/Module27.5/practice/Calculator/calculator.js
@@ -1,23 +1,27 @@
 
 // equal button
 document.getElementById('equal').addEventListener('click', function () {
-    const inputValue = getInputFieldValue('user-input');
-    if (inputValue == 0) {
-        alert('Please enter number');
-        return;
-    }
-    let num1 = 0;
-    let num2 = 0;
-    let operator='';
-    for (let i = 0; i < inputValue.length; i++) {
-        if (inputValue[i] == '+' || inputValue[i] == '-' || inputValue[i] == '*' || inputValue[i] == '/') {
-            num1 = parseFloat(inputValue.substring(0, i));
-            num2 = parseFloat(inputValue.substring(i + 1, inputValue.length));
-            operator = inputValue[i];
-            break;
-        }
-    }
-    const result = operation(num1, num2, operator);
-    setResult('equal-result', result);
+    filter('user-input');
+})
+// enter key press
+// see function onlyNumberInput
+
+// AC button
+document.getElementById('ac').addEventListener('click', function () {
+    allClear('user-input', 'equal-result');
 })
 
+// clear button
+document.getElementById('clear').addEventListener('click', function () {
+    clear('user-input');
+
+})
+
+// buttons
+const buttons = document.getElementsByClassName('btn')
+for (const btn of buttons) {
+    btn.addEventListener('click', function (event) {
+        const buttonValue = event.target.innerText;
+        setInput('user-input',buttonValue);
+    })
+}
--- Milestone5/Module27.5/practice/Calculator/utilities.js
+++ Milestone5/Module27.5/practice/Calculator/utilities.js
@@ -3,11 +3,15 @@ function onlyNumberInput(event) {
     const text = event.charCode;
     if ((text >= 48 && text <= 57) || text == 47 || text == 42 || text == 43 || text == 45) {
         return true;
+    } else if (text == 13) {
+        // This is for enter key press. and show result
+        filter('user-input');
     } else {
         return false;
     }
 }
 
+// input field value
 function getInputFieldValue(inputFieldId) {
     const inputText = document.getElementById(inputFieldId);
     if (inputText != '') {
@@ -18,6 +22,31 @@ function getInputFieldValue(inputFieldId) {
     }
 }
 
+
+// filter number and operator from input inputFieldId
+function filter(userInput) {
+    const inputValue = getInputFieldValue(userInput);
+    if (inputValue == 0) {
+        alert('Please enter number');
+        return;
+    }
+    let num1 = 0;
+    let num2 = 0;
+    let operator = '';
+    for (let i = 0; i < inputValue.length; i++) {
+        if (inputValue[i] == '+' || inputValue[i] == '-' || inputValue[i] == '*' || inputValue[i] == '/') {
+            num1 = parseFloat(inputValue.substring(0, i));
+            num2 = parseFloat(inputValue.substring(i + 1, inputValue.length));
+            operator = inputValue[i];
+            break;
+        }
+    }
+    const result = operation(num1, num2, operator);
+    setResult('equal-result', result);
+}
+
+
+// operations
 function operation(num1, num2, operation) {
     if (operation == '+') {
         return num1 + num2;
@@ -30,7 +59,31 @@ function operation(num1, num2, operation) {
     }
 }
 
+// set input in input inputField by page key
+function setInput(elementId, input) {
+    const element = document.getElementById(elementId);
+    element.value += input;
+}
+
+// set result in board
 function setResult(elementId,result) {
     const element = document.getElementById(elementId);
     element.innerText = `= ${result}`;
+}
+
+// clear input field
+function allClear(inputFieldId,elementId) {
+    const Text = document.getElementById(inputFieldId);
+    Text.value = '';
+    const result = document.getElementById(elementId);
+    result.innerText = '';
+}
+
+// clear button function
+function clear(userInput) {
+    const inputValue = getInputFieldValue(userInput);
+     const Value = inputValue.substring(0, inputValue.length - 1);
+
+    const oldValue = document.getElementById(userInput)
+    oldValue.value = Value;
 }
