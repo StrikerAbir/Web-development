b3b1f4d453f3d624c73b52fe5d8865f08c41d0d5
Abir Hasan <cse.170201063@gmail.com>
2022-08-23 15:57:18 +0600

    29.5.3 try catch

---
+++ Milestone5/Module29.5/try-catch.js
@@ -0,0 +1,18 @@
+const div= 'day the din';
+console.log(div);
+
+
+
+
+try {
+    console.log(district)
+} catch (error) {
+    console.log(error);
+} finally {
+    console.log('finally')
+}
+
+let district = 'bbaria'
+console.log(
+    61
+)
