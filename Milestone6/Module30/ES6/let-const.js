76a9091fc0404092807910b1d5bbe6551aa0487f
Abir Hasan <cse.170201063@gmail.com>
2022-08-24 17:00:52 +0600

    30.2 ES6 intro and var let const

---
+++ Milestone6/Module30/ES6/let-const.js
@@ -0,0 +1,15 @@
+let ok = 5;
+ok = 6;
+console.log(ok)
+
+const ok1 = 5;
+ok1 = 6;
+console.log(ok1)
+
+// we can push data in const array
+const array = [3, 4, 6, 5];
+console.log(array)
+array.push(8)
+array[1] = 30;
+console.log(array)
+array=[3,2,55,66]
