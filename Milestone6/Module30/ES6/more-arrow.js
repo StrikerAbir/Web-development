60dc4591371344b6b42693ee204687216074610b
Abir Hasan <cse.170201063@gmail.com>
2022-08-24 19:05:16 +0600

    30.6 more arrow function and big arrow function

---
+++ Milestone6/Module30/ES6/more-arrow.js
@@ -0,0 +1,22 @@
+// for one parameter 2 types
+// type 1
+const double = (num) => num * 2;
+// type 2 (sudhu parameter er brackets nai)
+const five = num => num * 5;
+
+// for 2 parameter
+const add = (a, b) => a + b;
+console.log(add(1, 2));
+
+// for multi parameter
+const addAll = (a, b, c, d) => a + b + c + d;
+
+// for no parameter
+const getPi = () => 3.1416;
+
+// multiline arrow function
+const doMath = (x, y, z) => {
+    const sum = x + y;
+    const multi = sum * Z;
+    return multi;
+}
