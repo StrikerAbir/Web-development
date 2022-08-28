34a035c8297c1595c898f0e448f8ed4872a226c0
Abir Hasan <cse.170201063@gmail.com>
2022-08-28 15:43:04 +0600

    33.4 call dynamic api, load and display dynamic data

--- Milestone6/Module33/Api Examples/contries.js
+++ Milestone6/Module33/Api Examples/contries.js
@@ -13,10 +13,25 @@ const displayData = (data) => {
         div.classList.add('country');
         div.innerHTML = `
             <h3>Name: ${country.name.common}</h3>
-            <p>Name: ${country.capital ? country.capital[0] :'No capital'}</p>
+            <p>Name: ${country.capital ? country.capital[0] : 'No capital'}</p>
+            <button onclick="loadDetails('${country.cca2}')">Details</button>
         `
         //tarnary operator use kora hoise karon onek country er capital nai.
         countries.appendChild(div)
     })
+}
+
+const loadDetails = (code) => {
+    // https://restcountries.com/v3.1/alpha/{code}    details er pattern emn
+    const url = `https://restcountries.com/v3.1/alpha/${code}`;
+    fetch(url).then((response) =>response.json()).then(data => displayDetails(data[0]) )
+} 
+
+const displayDetails = (data)=>{
+    const countryDetails = document.getElementById('country-detail');
+    countryDetails.innerHTML = `
+        <h2>${data.name.common}</h2>
+        <img src="${data.flags.png}">
+    `
 }
 loadData();
