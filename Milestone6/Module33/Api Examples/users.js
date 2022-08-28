1f13e4a5b27a86fe5d5af4cc3dfbd3442c993a9b
Abir Hasan <cse.170201063@gmail.com>
2022-08-28 17:59:58 +0600

    33.8 error handle fetch error, use try catch and async await

---
+++ Milestone6/Module33/Api Examples/users.js
@@ -0,0 +1,23 @@
+const loadUserFetch = () => {
+    const url = 'https://randomuser.me/api/?gender=female';
+    fetch(url)
+        .then(response => response.json())
+        .then(json => displayUser(json.results[0]))
+        .catch(err => console.log(err));
+}
+
+//fetch er kaj tai async dia evabe kora hoi.
+const loadUserAsync = async () => {
+    const url = 'https://randomuser.me/api/?gender=female';
+    try {
+        const response = await fetch(url);
+        const data = await response.json();
+        displayUser(data.results[0])
+    } catch (err) {
+        console.log(err)
+    }
+   
+}
+const displayUser = (data) => {
+    console.log(data);
+}
