762b1491abee9393aace976a036a4da4ce6cc6fc
Abir Hasan <cse.170201063@gmail.com>
2022-08-28 17:01:11 +0600

    33.6 display dynamic search result using bootstrap

--- Milestone6/Module33/Api Examples/meals.js
+++ Milestone6/Module33/Api Examples/meals.js
@@ -1,20 +1,21 @@
-const loadData = () => {
-    fetch('https://www.themealdb.com/api/json/v1/1/search.php?f=a').then((response) =>response.json()).then(data => displayData(data.meals))
+const loadData = (search) => {
+    const url =`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`
+    fetch(url).then((response) =>response.json()).then(data => displayData(data.meals))
 }
 const displayData = (data) => {
     const mealContainer = document.getElementById('meal-container');
+    mealContainer.innerHTML = ``; //eta na dile search er por sudhu ota soho ager gulo thakbe.
     data.forEach(meal => {
         const div = document.createElement('div');
         div.classList.add('col')
         div.innerHTML = `
             <div class="card">
-              <img src="..." class="card-img-top" alt="..." />
+              <img src="${meal.strMealThumb}" class="card-img-top" alt="..." />
               <div class="card-body">
-                <h5 class="card-title">Card title</h5>
+                <h5 class="card-title">${meal.strMeal}</h5>
                 <p class="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  
+                ${meal.strInstructions.slice(0,200)}
                 </p>
               </div>
             </div>
@@ -22,4 +23,10 @@ const displayData = (data) => {
         mealContainer.appendChild(div);
     })
 }
-loadData();
+
+const searchFood=()=>{
+    const searchField = document.getElementById('search-field');
+    const value = searchField.value;
+    loadData(value);
+    searchField.value = '';
+}
