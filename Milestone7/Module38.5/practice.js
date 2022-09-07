1a262e9769beef3735cfd06e9f7d443734fa99b1
Abir Hasan <cse.170201063@gmail.com>
2022-09-07 22:41:56 +0600

    level 2 task done

---
+++ Milestone7/Module38.5/practice.js
@@ -0,0 +1,44 @@
+// 1
+// setTimeout(function () { console.log('bane'); }, 3500);
+// 2
+// const value = prompt('enter a number');
+// const total = parseInt(value) + 200;
+// console.log(total);
+
+//3
+// const decision = confirm('Do you want to go MDN');
+// if (!!decision) {
+//     const location = location.href = 'https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/parseInt';
+//     console.log(location);
+// }
+
+//! level 2 task
+
+const getDataStorage = () => {
+    const data = localStorage.getItem('count');
+    let count = 0;
+    if (data) {
+        count = parseInt(data);
+    }
+    return count;
+}
+
+const setDataStorage = () => {
+    let data = getDataStorage();
+    let newData = ++data;
+    localStorage.setItem('count', newData);
+}
+
+const add = () => {
+    setDataStorage();
+    display()
+}
+
+const display = () => {
+    const count = document.getElementById('count');
+    const value = getDataStorage();
+    count.innerText = `${value}`;
+}
+display();
+
+//! level 3 task
