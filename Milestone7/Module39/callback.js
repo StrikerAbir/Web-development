ba0382a057b01dc34b8611fa6cdc067ffdb759bc
Abir Hasan <cse.170201063@gmail.com>
2022-09-08 21:46:21 +0600

    39.7 callback function

---
+++ Milestone7/Module39/callback.js
@@ -0,0 +1,16 @@
+function greetings(func,name) {
+    func(name);
+}
+
+function greetingHandler(name) {
+    console.log('good morning ',name);
+}
+
+function greetingEvening(name) {
+    console.log('good evening' ,name);
+}
+const name = 'bane';
+// greetings(name);
+greetings(greetingHandler, name);
+greetings(greetingEvening, name);
+
