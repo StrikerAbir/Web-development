f77111f78bea300ff7335e07d6500b29e300139b
Abir Hasan <cse.170201063@gmail.com>
2022-09-12 02:01:14 +0600

    42.1 six fundamental js

---
+++ Milestone7/Module42/JS concept for react/fundamental.js
@@ -0,0 +1,18 @@
+// 1. How to declare a variable using let and const
+const fathername = 'babul'
+let season = 'rainy'
+
+// 2. 6 basic Conditions >,<, ===, !==, <=, >=, &&, ||
+
+// 3. Array, index, length, push,
+// 4. Loop, for loop, for of, for in, for each
+// 5. Function
+// 6. object
+const object = {
+    name: 'Tishu',
+    relation: "Abir's love",
+}
+const variable = 'name';
+console.log(object.name); // direct by property
+console.log(object['relation']); // access via property name string
+console.log(object[variable]); // access via property name in variable.
