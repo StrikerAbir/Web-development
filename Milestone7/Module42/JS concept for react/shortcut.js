341a7c861de528417bf3e2933782d28e759dc74b
Abir Hasan <cse.170201063@gmail.com>
2022-09-13 17:09:12 +0600

    42.6 (Advanced) truthy, false ternary operator,  shortcut and or

---
+++ Milestone7/Module42/JS concept for react/shortcut.js
@@ -0,0 +1,28 @@
+// ternary
+let food = 60;
+let food1 = food > 100 ? 'biriani' : 'cha';
+console.log(food1);
+
+//convert num to string
+const b = 60;
+const re = 60 + '';
+console.log(re);
+
+// convert string to num
+const a = +re;
+console.log(a);
+
+// ternary shortcut
+const isActive = true;
+const showUser = () => console.log('display user');
+const hideUser = () => console.log('hide user');
+isActive ? showUser() : hideUser();
+
+//more shortcut
+//user && if the left side is true then right side will be executed
+isActive && showUser();
+// use || if the left side is false then right side will be executed
+isActive || hideUser();
+
+// toggle boolean
+isActive=!isActive;
