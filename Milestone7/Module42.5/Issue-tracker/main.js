4d6a7b09d061627ab31c845816a42452bac4f801
Abir Hasan <cse.170201063@gmail.com>
2022-09-13 22:38:57 +0600

    full fixed

--- Milestone7/Module42.5/Issue-tracker/main.js
+++ Milestone7/Module42.5/Issue-tracker/main.js
@@ -5,20 +5,25 @@ function submitIssue(e) {
   const description = getInputValue('issueDescription');
   const severity = getInputValue('issueSeverity');
   const assignedTo = getInputValue('issueAssignedTo');
-  const id = Math.floor(Math.random()*100000000) + '';
-  const status = 'Open';
+  if (description && severity && assignedTo) {
+    const id = Math.floor(Math.random() * 100000000) + '';
+    const status = 'Open';
 
-  const issue = { id, description, severity, assignedTo, status };
-  let issues = [];
-  if (localStorage.getItem('issues')){
-    issues = JSON.parse(localStorage.getItem('issues'));
+    const issue = { id, description, severity, assignedTo, status };
+    let issues = [];
+    if (localStorage.getItem('issues')) {
+      issues = JSON.parse(localStorage.getItem('issues'));
+    }
+    issues.push(issue);
+    localStorage.setItem('issues', JSON.stringify(issues));
+
+    document.getElementById('issueInputForm').reset();
+    fetchIssues();
+    e.preventDefault();
+  } else {
+    return;
   }
-  issues.push(issue);
-  localStorage.setItem('issues', JSON.stringify(issues));
-
-  document.getElementById('issueInputForm').reset();
-  fetchIssues();
-  e.preventDefault();
+  
 }
 
 const closeIssue = id => {
