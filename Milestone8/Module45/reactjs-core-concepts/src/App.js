0d2d50b8f4039d667949cc4d7fa4cdae28bf4269
Abir Hasan <cse.170201063@gmail.com>
2022-09-19 22:09:55 +0600

    45.2 JSX, dynamic content, dynamic style in react

--- Milestone8/Module45/reactjs-core-concepts/src/App.js
+++ Milestone8/Module45/reactjs-core-concepts/src/App.js
@@ -1,11 +1,34 @@
 import logo from './logo.svg';
 import './App.css';
 
+const number = 555;
+const singer= {name:'dr bane',job:'singer'}
+const singer2 = { name: 'yo yo bane', job: 'singer' }
+const singerStyle = {
+  color: 'purple',
+  backgroundColor:'white',
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>JSX</h1>
+        <div className="container">
+          <h3>hello dude. I am in react now.</h3>
+        </div>
+
+        <div className="music">
+          <p>No.: {number+2}</p>
+          <p style={singerStyle}>Name: {singer.name}, { singer.job }</p>
+          <p style={{
+            color: 'blue',
+            backgroundColor: 'yellow',
+          }}>Name: {singer2.name}, { singer2.job }</p>
+        </div>
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
