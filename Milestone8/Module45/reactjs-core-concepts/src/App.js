5650e8ecaf2ac638ed87b568cac0806d4b55a598
Abir Hasan <cse.170201063@gmail.com>
2022-09-20 02:33:05 +0600

    45.5 create multiple component from an array of object

--- Milestone8/Module45/reactjs-core-concepts/src/App.js
+++ Milestone8/Module45/reactjs-core-concepts/src/App.js
@@ -2,14 +2,20 @@ import logo from './logo.svg';
 import './App.css';
 
 const number = 555;
-const singer= {name:'dr bane',job:'singer'}
-const singer2 = { name: 'yo yo bane', job: 'singer' }
+const singers= [{name:'dr bane',job:'singer'},
+  { name: 'yo yo bane', job: 'singer' },
+  { name: 'yo yo popo', job: 'singer' },
+  { name: 'yo yo banbanti vaijan', job: 'singer' },
+]
 const singerStyle = {
   color: 'purple',
   backgroundColor:'white',
 }
 
 function App() {
+
+  const nayoks=['rubel','bappa','kuber','joshim','riaj','salman','anwar']
+
   return (
     <div className="App">
       {/* <header className="App-header">
@@ -29,12 +35,23 @@ function App() {
           }}>Name: {singer2.name}, { singer2.job }</p>
         </div>   
       </header> */}
+      {/* <Person name='bane' nayka='banu'></Person>
       <Person name='bane' nayka='banu'></Person>
       <Person name='popo' nayka='popu'></Person>
       <Person name='banerange' nayka='hululu'></Person>
       <h3>New component</h3>
       <Friend name='banerange pungaranga' phone='+1234'></Friend>
-      <Friend name='banbanti vaijan' phone='+2354'></Friend>
+      <Friend name='banbanti vaijan' phone='+2354'></Friend> */}
+
+      {/* {
+        nayoks.map(nayok => <li>Name: { nayok }</li>)
+      }
+      {
+        nayoks.map(nayok=> <Person name={nayok}></Person>)
+      } */}
+      {
+        singers.map(singer => <Person name={singer.name }></Person>)
+      }
     </div>
   );
 }
@@ -47,7 +64,7 @@ function Person(props) {
       {/* <h1>Sakib al Hasan</h1> */}
       <h1>{props.name}</h1>
       {/* <p>profession: cricketer</p> */}
-      <p>nayka: {props.nayka}</p>
+      {/* <p>nayka: {props.nayka}</p> */}
     </div>
 
     //way 2
