eff5c2347ab73d3f1cef271e39479fb1b24f47cd
Abir Hasan <cse.170201063@gmail.com>
2022-09-20 17:55:35 +0600

    45.8 advanced load dynamic data, api call useEffect integrate state.

--- Milestone8/Module45/recap-react/src/App.js
+++ Milestone8/Module45/recap-react/src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 function App() {
   const products = [
@@ -17,10 +17,43 @@ function App() {
         products.map((product) => <Product name={product.name} price={product.price}></Product>)
       } */}
 
-      <Counter></Counter>
+      {/* <Counter></Counter> */}
+
+      <ExternalUsers></ExternalUsers>
+
     </div>
   );
 }
+
+function ExternalUsers() {
+  // step1
+  const [users, setUsers] = useState([]);
+
+  // step2
+  useEffect(() => {
+    // step3
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(res => res.json())
+      .then(data=> setUsers(data))
+  }, [])
+  
+  return (
+    <div>
+      <h2>External users</h2>
+      {
+        users.map(user => <User name={user.name} email={user.email}></User>)
+      }
+    </div>
+  )
+}
+
+function User(props) {
+  return (
+    <div style={{ border: '2px solid black', margin:'20px'}}>
+      <h3>Name: {props.name}</h3>
+      <p>email: {props.email}</p>
+  </div>
+)}
 
 function Product(props) {
   return (
