d89a1968604feb416089e143467b56eaaf4bafb0
Abir Hasan <cse.170201063@gmail.com>
2022-09-24 05:37:12 +0600

    47.5 react hook works, send state via props props vs state

--- Milestone8/Module47/react-work/src/component/Watch/Watch.js
+++ Milestone8/Module47/react-work/src/component/Watch/Watch.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Display from '../Display/Display';
 
 const Watch = () => {
     const [steps, setSteps] = useState(0)
@@ -10,10 +11,11 @@ const Watch = () => {
       console.log(steps);
     }, [steps]);
     return (
-        <div>
+        <div style={{ border:'2px solid purple', margin:'20px'}}>
             <h1>New Watches</h1>
             <h3>My current steps :{steps}</h3>
             <button onClick={increase}>Step</button>
+            <Display name='bane' steps={steps}></Display>
         </div>
     );
 };
