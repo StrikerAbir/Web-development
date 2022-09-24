b31aaacf47c8dd01f9c6cb9de3e2a32f80970eef
Abir Hasan <cse.170201063@gmail.com>
2022-09-24 19:25:34 +0600

    47.8  reset class component

--- Milestone8/Module47/react-work/src/App.js
+++ Milestone8/Module47/react-work/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Watch from './component/Watch/Watch';
+import Tablet from './component/Tablet/Tablet';
 
 function App() {
   return (
     <div className="App">
       <Watch></Watch>
+      <Tablet></Tablet>
     </div>
   );
 }
---
+++ Milestone8/Module47/react-work/src/component/Tablet/Tablet.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Tablet extends Component {
+    render() {
+        return (
+            <div>
+                <h3>THis is tablet</h3>
+                <p>class component.</p>
+            </div>
+        );
+    }
+}
+
+export default Tablet;
