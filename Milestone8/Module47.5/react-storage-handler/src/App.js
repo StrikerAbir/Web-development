cc8e38c6e1523914053bd216c91bb7bf8cfe1b2d
Abir Hasan <cse.170201063@gmail.com>
2022-09-24 20:56:06 +0600

    47.5.1 import

---
+++ Milestone8/Module47.5/react-storage-handler/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Cosmo from './component/Cosmo/Cosmo';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Just now</h1>
+      <Cosmo></Cosmo>
+    </div>
+  );
+}
+
+export default App;
---
+++ Milestone8/Module47.5/react-storage-handler/src/component/Cosmo/Cosmo.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Cosmo = () => {
+    return (
+        <div>
+            <h2>COsmo</h2>
+        </div>
+    );
+};
+
+export default Cosmo;
