c1d7d5562d810d807b77e5aaf761bb43f9693686
Abir Hasan <cse.170201063@gmail.com>
2022-09-26 04:03:06 +0600

    48.5 Style product cart, style debug and button position

--- Milestone8/Module48/ema-john-simple/src/components/Product/Product.js
+++ Milestone8/Module48/ema-john-simple/src/components/Product/Product.js
@@ -3,10 +3,22 @@ import './product.css'
 const Product = (props) => {
     const {img, name, price, seller, ratings}=props.product
     return (
-        <div className="product">
-            <img src={img} alt="" />
-            <p className='product-name'>{name}</p>
+      <div className="product">
+        <img src={img} alt="" />
+        <div className="product-info">
+          <p className="product-name">{name}</p>
+          <p>Price: ${price}</p>
+          <p>
+            <small>Seller: {seller} stars</small>
+          </p>
+          <p>
+            <small>Ratings: {ratings} stars</small>
+          </p>
         </div>
+        <button className="btn-cart">
+          <p>Add to Cart</p>
+        </button>
+      </div>
     );
 };
 
