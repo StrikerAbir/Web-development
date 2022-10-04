7119d1cf746447c256cccfd9ffeaf1ab6eaca3d9
Abir Hasan <cse.170201063@gmail.com>
2022-10-05 03:21:05 +0600

    51.3 tailwind

---
+++ Milestone9/Module51/price-club/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl font-bold'>this is a big big h1</h1>
+      <p>just a para</p>
+    </div>
+  );
+}
+
+export default App;
