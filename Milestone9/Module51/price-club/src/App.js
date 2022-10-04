7e1060a45995e3eb3ee1a6d6c2708a4791dee188
Abir Hasan <cse.170201063@gmail.com>
2022-10-05 04:51:26 +0600

    51.5 nav toggle and navbar animation

--- Milestone9/Module51/price-club/src/App.js
+++ Milestone9/Module51/price-club/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import NavBar from './component/NavBar/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1 className='text-4xl font-bold'>this is a big big h1</h1>
       <p>just a para</p>
+
     </div>
   );
 }
