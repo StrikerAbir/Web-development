2c40a606c10ef738c96c1f65105c765ffbbbe626
Abir Hasan <cse.170201063@gmail.com>
2022-10-12 22:15:58 +0600

    data load and single data load

---
+++ Milestone9/Module55.5/iphone-hunter/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const app = express();
+const port = 1000;
+const phones = require('./phones.json')
+
+app.get('/', (req, res) => {
+    res.send('iphone: is here server now running')
+})
+
+// phone data api
+app.get('/phones', (req, res) => {
+    res.send(phones)
+})
+
+// single data loaded
+app.get('/phones/:id', (req, res) => {
+  const id = parseInt(req.params.id); // request use hoi kono dynamic value nite
+
+  console.log("looking for data for id:", id);
+
+  const phone = phones.find((ph) => ph.id === id) || {};
+  res.send(phone); //respond use hoi show korate
+})
+
+
+app.listen(port, () => {
+    console.log('port chole');
+})
--- Milestone9/Module55.5/myfirstserver/index.js
+++ Milestone9/Module55.5/myfirstserver/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
-const app = express();
+const express = require("express"); // express import
+const app = express(); // express call
 // const port = process.env.PORT || 1000;  etaw use kore,
-const port = 1000;
+const port = 1000; // port set
 
-app.get('/', (req, res) => {
-    res.send('Hello|World')
-})
+// api banao
+app.get("/", (req, res) => {
+  // get korbe ‘/’, and and akta callback function jar paramete akta request r akta respond
+  res.send("Hello|World"); //respond a hellow world send kore disi
+});
 
 app.listen(port, () => {
-    console.log(`server running ${port}`)
-})
+  // ekhane port dite hoi r chaile arrow funciton o dite pari jate bojha jai j port kaj korse kina.
+  console.log(`server running ${port}`);
+});
