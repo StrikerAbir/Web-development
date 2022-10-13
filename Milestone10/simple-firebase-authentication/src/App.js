94d6c2f211a2aae74ac31863479af66954f644b2
Abir Hasan <cse.170201063@gmail.com>
2022-10-13 20:43:15 +0600

    56.5 Display logged in user info, sign out, toggle sign in, sign out

--- Milestone10/simple-firebase-authentication/src/App.js
+++ Milestone10/simple-firebase-authentication/src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
-import app from './firebase/firebase.init';
+import logo from "./logo.svg";
+import "./App.css";
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
+import app from "./firebase/firebase.init";
+import { useState } from "react";
 
 const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+
   const provider = new GoogleAuthProvider();
   const handleGoogleSignIn = () => {
     signInWithPopup(auth, provider)
-      .then(result => {
+      .then((result) => {
         const user = result.user;
+        setUser(user);
         console.log(user);
+        // console.log(user.photoURL);
       })
-      .catch(error => {
-      console.error('error',error);
-    })
+      .catch((error) => {
+        console.error("error", error);
+      });
+  };
+
+  const handleGoogleSignOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        console.error("error", error);
+      });
   };
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIn}>Google sign in</button>
+      {user.email ? (
+        <button onClick={handleGoogleSignOut}>Google sign out</button>
+      ) : (
+        <button onClick={handleGoogleSignIn}>Google sign in</button>
+      )}
+      {/* conditional rendering */}
+      {user.email && (
+        <div>
+          <h3>User Name: {user.displayName}</h3>
+          <p>Email: {user.email}</p>
+          <img src={user.photoURL} alt="" />
+        </div>
+      )}
     </div>
   );
 }
