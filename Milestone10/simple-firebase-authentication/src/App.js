0f45b069ed8abc0ae05e850c048030b31cdfd3f0
Abir Hasan <cse.170201063@gmail.com>
2022-10-13 19:34:32 +0600

    56-4 Open google login popup and Recap google popup sign in

--- Milestone10/simple-firebase-authentication/src/App.js
+++ Milestone10/simple-firebase-authentication/src/App.js
@@ -1,14 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
-import { getAuth, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import app from './firebase/firebase.init';
 
 const auth = getAuth(app);
-const handleGoogleSignIn = () => {
-  console.log('google coming soon');
-}
+
 function App() {
-const provider=new GoogleAuthProvider()
+  const provider = new GoogleAuthProvider();
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, provider)
+      .then(result => {
+        const user = result.user;
+        console.log(user);
+      })
+      .catch(error => {
+      console.error('error',error);
+    })
+  };
   return (
     <div className="App">
       <button onClick={handleGoogleSignIn}>Google sign in</button>
