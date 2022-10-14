8c045d9df88a318454d9adfe8809abac55560a54
Abir Hasan <cse.170201063@gmail.com>
2022-10-14 20:41:10 +0600

    57.4 Create new user using email and password on firebase

--- Milestone10/email-password-auth/src/components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ Milestone10/email-password-auth/src/components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+import app from '../../firebase/firebase.init';
 
+const auth = getAuth(app);
 const RegisterReactBootstrap = () => {
     const handleRegister = (event) => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
-        console.log(email);
-        console.log(password);
+        createUserWithEmailAndPassword(auth, email, password)
+            .then(result => {
+                const user=result.user
+            console.log(user);
+            })
+            .catch(err => {
+            console.error('error',err);
+        })
     }
     return (
         <div className='w-50 mx-auto'>
@@ -16,12 +25,12 @@ const RegisterReactBootstrap = () => {
         <Form onSubmit={handleRegister}>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" name='email'/>
+            <Form.Control type="email" placeholder="Enter email" name='email' required/>
           </Form.Group>
 
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" name='password'/>
+            <Form.Control type="password" placeholder="Password" name='password' required/>
           </Form.Group>
           <Button variant="primary" type="submit">
             Register
