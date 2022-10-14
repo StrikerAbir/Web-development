7773a824a1c79172eb15d830d8efd19b5ae2d80a
Abir Hasan <cse.170201063@gmail.com>
2022-10-14 21:22:47 +0600

    57.5 Login form field validation using regular expression.

--- Milestone10/email-password-auth/src/components/RegisterReactBootstrap/RegisterReactBootstrap.js
+++ Milestone10/email-password-auth/src/components/RegisterReactBootstrap/RegisterReactBootstrap.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
@@ -6,18 +6,34 @@ import app from '../../firebase/firebase.init';
 
 const auth = getAuth(app);
 const RegisterReactBootstrap = () => {
+
+    const [passErr,setPassErr]=useState('')
     const handleRegister = (event) => {
         event.preventDefault();
         const email = event.target.email.value;
         const password = event.target.password.value;
-        createUserWithEmailAndPassword(auth, email, password)
-            .then(result => {
-                const user=result.user
-            console.log(user);
+
+        if (!/(?=.*[A-Z].*[A-Z])/.test(password)) {
+            setPassErr('Please provide at least two uppercase');
+            return;
+        }
+        if (password.length < 6) {
+            setPassErr("Password must be at least 6 characters long");
+            return;
+        }
+        if (!/(?=.*[!#$%&? "])/.test(password)) {
+            setPassErr('Please add one special character');
+            return;
+        }
+        setPassErr('');
+          createUserWithEmailAndPassword(auth, email, password)
+            .then((result) => {
+              const user = result.user;
+              console.log(user);
             })
-            .catch(err => {
-            console.error('error',err);
-        })
+            .catch((err) => {
+              console.error("error", err);
+            });
     }
     return (
         <div className='w-50 mx-auto'>
@@ -31,7 +47,8 @@ const RegisterReactBootstrap = () => {
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
             <Form.Control type="password" placeholder="Password" name='password' required/>
-          </Form.Group>
+                </Form.Group>
+                <p className='text-danger'>{ passErr}</p>
           <Button variant="primary" type="submit">
             Register
           </Button>
