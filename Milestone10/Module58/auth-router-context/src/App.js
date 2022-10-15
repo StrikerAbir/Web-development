f6923dbf0922f47f463a67938fcf1a5103fb0613
Abir Hasan <cse.170201063@gmail.com>
2022-10-16 02:37:17 +0600

    58.1 setup

---
+++ Milestone10/Module58/auth-router-context/src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layout/Main';
+import Home from './component/Home/Home';
+import Login from './component/Login/Login';
+import Register from './component/Register/Register';
+
+function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: "/",
+          element: <Home></Home>
+        },
+        {
+          path: "/login",
+          element: <Login></Login>
+        },
+        {
+          path: "/register",
+          element: <Register></Register>
+        },
+
+      ]
+    }
+  ]); 
+  return (
+    <div className="App">
+      <RouterProvider router={router}></RouterProvider>
+    </div>
+  );
+}
+
+export default App;
