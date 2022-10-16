6b6bfc40dedae7eeab42ac5ad4973bd4e18e1b53
Abir Hasan <cse.170201063@gmail.com>
2022-10-16 15:42:44 +0600

    58.3 Create user context for shared authentication

---
+++ Milestone10/Module58/auth-router-context/src/firebase/firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDh0rCq0NOXUhPgq6YIzhipzJPxRkZ0H6k",
+  authDomain: "auth-router-context-8fba6.firebaseapp.com",
+  projectId: "auth-router-context-8fba6",
+  storageBucket: "auth-router-context-8fba6.appspot.com",
+  messagingSenderId: "44149063012",
+  appId: "1:44149063012:web:7bfe3770b6703405d50e71"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
