af13e29732a6aaeff9f1f34b831b952e86671e36
Abir Hasan <cse.170201063@gmail.com>
2022-10-30 07:19:20 +0600

    64.7 Send data to database using mongodb insertOne

--- Milestone11/Module64/simple node server/index.js
+++ Milestone11/Module64/simple node server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = process.env.PORT || 1000;
-const cors = require('cors');
+const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 app.use(cors());
@@ -9,56 +9,76 @@ app.use(express.json());
 
 // user : dbUser1
 // password : fLE1x3B5DBR1DZPt
-
-
 const uri =
   "mongodb+srv://dbUser1:fLE1x3B5DBR1DZPt@cluster0.nvx6pod.mongodb.net/?retryWrites=true&w=majority";
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
-client.connect((err) => {
-  const collection = client.db("simpleNode").collection("users");
-  // perform actions on the collection object
-    console.log('database connected');
-  client.close();
+
+async function run() {
+  try {
+    const userCollection = client.db("simpleNode").collection("users");
+    // const user = { name: "mihit", email: "12@gmail.com" };
+    // const result = await userCollection.insertOne(user);
+    // console.log(result);
+      app.post("/users", async (req, res) => {
+        console.log("user post api called");
+        const user = req.body;
+        // user.id = users.length + 1;
+        // users.push(user);
+          const result = await userCollection.insertOne(user);
+          console.log(result);
+          user.id = result.insertedId;
+        res.send(user);
+      });
+  } finally {
+  }
+}
+
+run().catch((err) => console.log(err));
+
+// client.connect((err) => {
+//   const collection = client.db("simpleNode").collection("users");
+//   // perform actions on the collection object
+//   console.log("database connected");
+//   client.close();
+// });
+
+app.get("/", (req, res) => {
+  res.send("simple node server running");
 });
-
-
-
-app.get('/', (req, res) => {
-    res.send('simple node server running');
-})
 
 const users = [
-    {id:1, name:'hinata', email:'hinataWifu@gmail.com'},
-    {id:1, name:'zero2', email:'zero2Wifu@gmail.com'},
-    {id:1, name:'hancock', email:'hancockWifu@gmail.com'},
-]
+  { id: 1, name: "hinata", email: "hinataWifu@gmail.com" },
+  { id: 1, name: "zero2", email: "zero2Wifu@gmail.com" },
+  { id: 1, name: "hancock", email: "hancockWifu@gmail.com" },
+];
 
 app.get("/users", (req, res) => {
-    if (req.query.name) {
-        // filter by query
-        const search= req.query.name
-        const filtered = users.filter(user => user.name.toLowerCase().indexOf(search)>=0)
-        res.send(filtered);
-    } else {
-        
-        res.send(users);
-    }
+  if (req.query.name) {
+    // filter by query
+    const search = req.query.name;
+    const filtered = users.filter(
+      (user) => user.name.toLowerCase().indexOf(search) >= 0
+    );
+    res.send(filtered);
+  } else {
+    res.send(users);
+  }
 });
 
-app.post('/users', (req, res) => {
-    console.log('user post api called');
-    const user = req.body;
-    user.id = users.length + 1;
-    users.push(user);
-    res.send(user)
-})
-
+// app.post("/users", (req, res) => {
+//   console.log("user post api called");
+//   const user = req.body;
+//   user.id = users.length + 1;
+//   users.push(user);
+//   res.send(user);
+// });
 
 // listen
 app.listen(port, () => {
-    console.log(`port ${port} running...`);
-})
+  console.log(`port ${port} running...`);
+});
