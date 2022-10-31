49d4e89661d69e9305b6c0871b661a914f57e528
Abir Hasan <cse.170201063@gmail.com>
2022-10-31 18:31:57 +0600

    65.6 Delete a user from the database and show remaining users

--- Milestone11/Module65/node-mongo-crud-client/src/component/Home/Home.js
+++ Milestone11/Module65/node-mongo-crud-client/src/component/Home/Home.js
@@ -1,14 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData();
+    const [displayUsers, setDisplayUsers] = useState(users);
 
     const handleDelete = user => {
         const agree =window.confirm(`Are you sure you want to delete: ${user.name}`)
         if (agree) {
-            
-            console.log('deleting usr with', user._id);
+            fetch(`http://localhost:1000/user/${user._id}`, {
+              method: "DELETE",
+            })
+              .then((response) => response.json())
+                .then((data) => {
+                    if (data.deletedCount > 0) {
+                        alert('user deleted successfully');
+                        const remainingUsers = displayUsers.filter(usr => usr._id !== user._id)
+                        setDisplayUsers(remainingUsers);
+                    }
+                    console.log(data)
+                });
         }
     }
 
@@ -18,9 +29,9 @@ const Home = () => {
           <Link to="/">Home</Link>
          <br /> <Link to="/user/add">Add user</Link>
         </div>
-        <h2>USers : {users.length}</h2>
+        <h2>USers : {displayUsers.length}</h2>
         <div>
-          {users.map((user) => (
+          {displayUsers.map((user) => (
             <p key={user._id}>
               {user.name} {user.email} <button onClick={()=>handleDelete(user)}>X</button>
             </p>
--- Milestone11/Module65/node-mongo-crud-server/index.js
+++ Milestone11/Module65/node-mongo-crud-server/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 1000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -22,19 +22,30 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         const userCollection = client.db('nodeMongoCrud').collection('users');
-
+      // read
       app.get('/users', async (req, res) => {
         const query = {};
         const cursor = userCollection.find(query);
         const user = await cursor.toArray();
         res.send(user);
       })
+
+      // create
       app.post('/users', async(req, res) => {
         const user = req.body;
         console.log(user);
         const result =await userCollection.insertOne(user)
         res.send(result);
-        })
+      })
+      
+      // delete
+      app.delete('/user/:id', async (req, res) => {
+        const id = req.params.id;
+        console.log(id);
+        const query = { _id: ObjectId(id) }
+        const result = await userCollection.deleteOne(query)
+        res.send(result);
+      })
     } finally {
        
     }
