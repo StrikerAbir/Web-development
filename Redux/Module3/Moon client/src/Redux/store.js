c22b209aeead4587e094a5ea168032f9918b2681
Abir Hasan <cse.170201063@gmail.com>
2023-03-27 03:41:50 +0600

    dev tool 3.4

--- Redux/Module3/Moon client/src/Redux/store.js
+++ Redux/Module3/Moon client/src/Redux/store.js
@@ -1,6 +1,8 @@
+
+import { composeWithDevTools } from "@redux-devtools/extension/lib/types/logOnly"
 import {createStore} from "redux"
 import productReducer from "./Reducer/productReducer"
-
-const store = createStore(productReducer)
+ 
+const store = createStore(productReducer,composeWithDevTools())
 
 export default store
--- Redux/Module3/Moon client/src/pages/Cart.js
+++ Redux/Module3/Moon client/src/pages/Cart.js
@@ -1,12 +1,16 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import ProductCard from "../components/ProductCard";
 
 
 const Cart = () => {
- 
+ const cart=useSelector(state=>state.cart)
 
   return (
     <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 max-w-7xl gap-14 mx-auto my-10'>
-      cart
+      {
+        cart.map(product=><ProductCard key={product.key} product={product}></ProductCard>)
+      }
     </div>
   );
 };
