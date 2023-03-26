9a677918976ceb5f53923d3dcf5f7fc463b53eb3
Abir Hasan <cse.170201063@gmail.com>
2023-03-27 01:29:42 +0600

    3.3 Action types and Action Creators.

--- Redux/Module3/Moon client/src/components/ProductCard.js
+++ Redux/Module3/Moon client/src/components/ProductCard.js
@@ -1,8 +1,11 @@
 import React from "react";
 import { BiListPlus } from "react-icons/bi";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../Redux/actionCreators/productAction";
+import { ADD_TO_CART } from "../Redux/actionTypes/actionTypes";
 
 const ProductCard = ({ product }) => {
-  
+  const dispatch = useDispatch();
 
   return (
     <div
@@ -24,8 +27,11 @@ const ProductCard = ({ product }) => {
       <div className='flex gap-2 mt-5'>
         <button
           className='bg-indigo-500 rounded-full py-1 px-2 flex-1 text-white text-bold'
-        
+        //   onClick={() => dispatch({
+        //     type: ADD_TO_CART, payload: product})}
+        onClick={addToCart(product)}
         >
+          
           Add to cart
         </button>
         <button
